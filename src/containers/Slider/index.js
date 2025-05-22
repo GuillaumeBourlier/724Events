ffa48011502ed45b3d2ee9bab11a2e24c1979637
VotreNom <VotreEmail@example.com>
2025-05-22 11:36:49 +0200

    Correction Fonctionnement Slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,25 +7,23 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  );
-  const nextCard = () => {
-    setTimeout(() => setIndex(index < byDateDesc.length ? index + 1 : 0), 5000);
-  };
-  useEffect(() => {
-    nextCard();
-  });
+
+const byDateDesc = data?.focus
+  ? [...data.focus].sort((evtA, evtB) => new Date(evtB.date) - new Date(evtA.date)) // tri par date décroissante le tableau
+  : [];
+
+  useEffect(() => { // Utilsation de useEffect pour faire défiler les événements toutes les 5 secondes
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % byDateDesc.length);
+    }, 5000);
+        return () => clearInterval(interval);
+  }, [byDateDesc]);
+
   return (
-    <div className="SlideCardList">
+    <div className="SlideCardList"> 
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
+          <div key={event.id || idx} // Ajout d'une clé unique
+            className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}>
             <img src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
@@ -35,22 +33,24 @@ const Slider = () => {
               </div>
             </div>
           </div>
+          )
+          )}
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
+              {byDateDesc.map((event, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={`${event.id ||`radio-${radioIdx}`}`} // Ajout d'une clé unique
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
-      ))}
+                  checked={index === radioIdx}
+                  onChange={() => setIndex(radioIdx)} // Ajout de la fonction onChange pour changer l'index des radios (bouton radio pour le slider)
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Slider;
+
